b9ce14629ade531b23d123682d05fdff6292f9e9
Mahesh-par <mahesh.mishra@10turtle.com>
2026-02-16 19:01:36 +0530

    testing123

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { registerUser, loginUser } from "../controllers/user.controller.js";
+import createBlog from "../controllers/post.controller.js"
 
 const router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/createBlog",createBlog) 
 
 
 export default router
