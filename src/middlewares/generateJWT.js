eb270d98900f50f1407515a79b71f91138a214cb
Mahesh-par <mahesh.mishra@10turtle.com>
2026-02-17 11:26:56 +0530

    console-log

--- src/middlewares/generateJWT.js
+++ src/middlewares/generateJWT.js
@@ -5,9 +5,6 @@ export const generateTokenAUthUser = (user)=>{
     expiresIn: "7d",
   });
   console.log(token,"------------token");
-  console.log(token,"------------tokeasdadadn");
-  console.log(token,"------------tokasdasdasdasden");
-  console.log(token,"------------tokasdasdasdasen");
-  console.log(token,"------------tokdasdasdasdasden");
+ 
   return token
 };
